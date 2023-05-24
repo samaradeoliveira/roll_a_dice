76dd7fbe9035cd318ebab7c0625b2fc026e275c4
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-05-24 15:44:57 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,57 @@
+#missão 1: importar corretamente o módulo (aleatório)
+#import RANDOM
+#import radomw 
+#import random 
+  
+# Use uma variável para armazenar a resposta para continuar jogando o dado
+response = "y"
+   
+   #verificação de valor em um laço de repetição
+while response == "y": 
+      
+    # Gere um número aleatório 
+    # entre 1 e 6 (incluindo 
+    # os números 1 e 6) 
+    
+
+    # condições para verificar o valor numérico  
+    if no == 1: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[  0  ]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 2: 
+        print("[-----]") 
+        print("[ 0   ]") 
+        print("[     ]") 
+        print("[   0 ]") 
+        print("[-----]") 
+    if no == 3: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 4: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[     ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 5: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[  0  ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 6: 
+        print("[-----]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+          
+    # Peça para o usuário informar uma resposta      
+    
+   
